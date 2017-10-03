fc5f9d19dc4af9a5c7601518d0e81645188653ad
Christo <christo@cryptodrunks.net>
2017-10-03 14:30:51 +0200

    refactor(plugin-csv): Use combinators-p over the sugarcube combinators.

--- packages/plugin-csv/lib/plugins/import.js
+++ packages/plugin-csv/lib/plugins/import.js
@@ -2,12 +2,12 @@ import {flow, map, concat, merge, get} from "lodash/fp";
 import Promise from "bluebird";
 import parse from "csv-parse";
 import fs from "fs";
+import {flow as flowP, fold as foldP} from "combinators-p";
 import {envelope as env, data as d, plugin as p, utils} from "@sugarcube/core";
 
 import {assertIdFields} from "../assertions";
 
 const {unfold} = utils.fs;
-const {reduceP} = utils.combinators;
 
 const querySource = "glob_pattern";
 
@@ -21,42 +21,46 @@ const importPlugin = (envelope, {log, cfg}) => {
     _sc_id_fields: idFields,
   });
 
-  return reduceP(
-    (memo, pattern) =>
-      unfold(pattern).then(
-        flow([
-          // The order of the merge matters, otherwise the id_fields are merged
-          // ssbadly.
-          map(u => merge(u, entity)),
-          concat(memo),
-        ])
+  return flowP(
+    [
+      foldP(
+        (memo, pattern) =>
+          unfold(pattern).then(
+            flow([
+              // The order of the merge matters, otherwise the id_fields are merged
+              // ssbadly.
+              map(u => merge(u, entity)),
+              concat(memo),
+            ])
+          ),
+        []
       ),
-    [],
-    patterns
-  )
-    .reduce((memo, unit) => {
-      const records = [];
-      const parser = parse({delimiter, columns: true});
-      const input = fs.createReadStream(unit.location);
+      foldP((memo, unit) => {
+        const records = [];
+        const parser = parse({delimiter, columns: true});
+        const input = fs.createReadStream(unit.location);
 
-      // eslint-disable-next-line promise/avoid-new
-      return new Promise((resolve, reject) => {
-        parser.on("error", reject);
-        parser.on("finish", () => {
-          log.info(`Finished parsing ${unit.location}`);
-          resolve(records);
-        });
-        parser.on("readable", () => {
-          let record;
-          // eslint-disable-next-line no-cond-assign
-          while ((record = parser.read())) {
-            records.push(merge(unit, record));
-          }
-        });
-        input.pipe(parser);
-      }).then(concat(memo));
-    }, [])
-    .then(xs => env.concatData(xs, envelope));
+        // eslint-disable-next-line promise/avoid-new
+        return new Promise((resolve, reject) => {
+          parser.on("error", reject);
+          parser.on("finish", () => {
+            log.info(`Finished parsing ${unit.location}`);
+            resolve(records);
+          });
+          parser.on("readable", () => {
+            let record;
+            // eslint-disable-next-line no-cond-assign
+            while ((record = parser.read())) {
+              records.push(merge(unit, record));
+            }
+          });
+          input.pipe(parser);
+        }).then(concat(memo));
+      }, []),
+      xs => env.concatData(xs, envelope),
+    ],
+    patterns
+  );
 };
 
 const plugin = p.liftManyA2([assertIdFields, importPlugin]);
