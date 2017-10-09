2340e6a9e017382d052b87741d3824e4cb94995a
Christo <christo@cryptodrunks.net>
2017-10-09 21:05:22 +0200

    refactor(plugin-tor): Replaced bluebird with combinators-p and pify.

--- packages/plugin-tor/lib/check.js
+++ packages/plugin-tor/lib/check.js
@@ -1,18 +1,22 @@
-import Promise from "bluebird";
+import {flowP, tapP} from "combinators-p";
+import pify from "pify";
 import request from "request";
 import cheerio from "cheerio";
 
-Promise.promisifyAll(request);
 const uri = "https://check.torproject.org/";
 
 const plugin = (envelope, {log}) =>
-  request
-    .getAsync(uri)
-    .tap(({body}) => {
-      const $ = cheerio.load(body);
-      log.info($("title").text());
-    })
-    .return(envelope);
+  flowP(
+    [
+      pify(request.get),
+      tapP(({body}) => {
+        const $ = cheerio.load(body);
+        log.info($("title").text());
+      }),
+      () => envelope,
+    ],
+    uri
+  );
 
 plugin.desc = "Check if HTTP is happening over the TOR network.";
 
