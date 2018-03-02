6892c52b4f57c9284648b307dd0f1663e74e22bf
Christo <christo@cryptodrunks.net>
2018-03-02 22:59:15 +0100

    feat(tap): Have fine grained control over what to print.

--- packages/plugin-tap/README.md
+++ packages/plugin-tap/README.md
@@ -21,6 +21,14 @@ to the screen.
 
 **Configuration**:
 
+- `tap.select` :: Specify the elements to print to the screen. Defaults to
+  `data`. Valid values are `data`, `queries`, `stats`, `cache`, `plugins` and
+  `cfg`. Specify multiple values by separating values by a comma.
+
+  Example: Print only data and queries to the screen.
+
+  `$(npm bin)/sugarcube -p tap_printf --tap.select data,queries`
+
 - `tap.limit` :: Limit the output to <n> data units.
 
    Example: Print only 5 units of data.
--- packages/plugin-tap/lib/printf.js
+++ packages/plugin-tap/lib/printf.js
@@ -1,24 +1,55 @@
 /* eslint-disable no-console */
-import {getOr, take} from "lodash/fp";
+import {omit, getOr, take} from "lodash/fp";
 import {inspect} from "util";
 
-const printf = (envelope, {stats, cfg}) => {
+const printf = (envelope, {stats, cfg, cache}) => {
   const {data, queries} = envelope;
+  const {plugins} = cfg;
   const limit = getOr(null, "tap.limit", cfg);
+  // FIXME: The default from printf.argv doesn't work here.
+  const selection = getOr("data", "tap.select", cfg).split(",");
+  const printables = {
+    data,
+    queries,
+    stats,
+    cache,
+    plugins,
+    cfg: omit(["queries", "stats", "cache", "plugins"], cfg),
+  };
   const opts = {colors: true, depth: null, maxArrayLength: null};
 
-  console.log("Data:");
-  console.log(inspect(limit ? take(limit, data) : data, opts));
-  console.log("\nQueries:");
-  console.log(inspect(queries, opts));
-  console.log("\nConfig:");
-  console.log(inspect(cfg, opts));
-  console.log("\nStats:");
-  console.log(inspect(stats.get(), opts));
+  selection.forEach(s => {
+    console.log(`${s}:`);
+    switch (s) {
+      case "data": {
+        console.log(inspect(limit ? take(limit, data) : data, opts));
+        break;
+      }
+      case "stats": {
+        console.log(inspect(stats.get(), opts));
+        break;
+      }
+      case "cache": {
+        console.log(inspect(cache.get(), opts));
+        break;
+      }
+      default:
+        console.log(inspect(printables[s], opts));
+    }
+  });
 
   return envelope;
 };
 
 printf.desc = "Print the envelope to the screen.";
+
+printf.argv = {
+  "tap.select": {
+    type: "string",
+    nargs: 1,
+    default: "data",
+    desc: "Specify what to log: data,queries,cfg,stats,cache,plugins",
+  },
+};
 
 export default printf;
