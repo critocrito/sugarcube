1a43e941a1d05cfc83173ef7347574db374f1c26
Christo <christo@cryptodrunks.net>
2018-08-06 16:20:01 +0200

    feat(plugin-ddg): Set user agent and pick correct href.

--- packages/plugin-ddg/lib/index.js
+++ packages/plugin-ddg/lib/index.js
@@ -1,82 +1,103 @@
-var _ = require('lodash'),
-    pify = require("pify"),
-    cheerio = require('cheerio'),
-    dashp = require('dashp'),
-    envelope = require('@sugarcube/core').envelope,
-    getAsync = pify(require('request').get),
-    url = require('url');
+var _ = require("lodash"),
+  pify = require("pify"),
+  cheerio = require("cheerio"),
+  dashp = require("dashp"),
+  envelope = require("@sugarcube/core").envelope,
+  // getAsync = pify(require("request").get),
+  request = require("request"),
+  url = require("url");
 
+var customRequest = request.defaults({
+  headers: {
+    "User-Agent":
+      "Mozilla/5.0 (X11; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0",
+  },
+});
+var getAsync = pify(customRequest);
 var tapP = dashp.tap;
 var collectP = dashp.collect;
 var flowP = dashp.flow;
 
-var sharedFields = ['href', 'description', 'title'];
+var sharedFields = ["href", "description", "title"];
 
 var duckClean = function(dirtyContent) {
   /* because contains a lot of spaces and \n\n */
-  return dirtyContent.replace(/\ \ \.*/g, '').replace(/\n/g, '');
+  return dirtyContent.replace(/\ \ \.*/g, "").replace(/\n/g, "");
 };
 
 var goGoDuck = function(searchQuery) {
+  var uri = "https://duckduckgo.com/html/?q=" + encodeURIComponent(searchQuery);
 
-  var uri = 'https://duckduckgo.com/html/?q=' + encodeURIComponent(searchQuery);
+  return getAsync(uri).then(function(response) {
+    if (response.statusCode !== 200) {
+      throw new Error("DuckDuckGo return " + response.statusCode);
+    }
+    var parsed_links = [];
+    var ddgEntry = cheerio.load(response.body)("div .links_main");
 
-  return getAsync(uri)
-    .then( function(response) {
-      if (response.statusCode !== 200) {
-        throw new Error("DuckDuckGo return " + response.statusCode);
-      }
-      var parsed_links = [];
-      var ddgEntry = cheerio.load(response.body)('div .links_main');
+    _.each(ddgEntry, function(div, i) {
+      var completeSection = cheerio.load(div).html();
+      var entryDesc = cheerio.load(div)(".result__snippet").text();
+      var entryTitle = cheerio.load(div)(".result__title").text();
+      var href = cheerio.load(div)(".result__a").attr("href");
 
-      _.each(ddgEntry, function(div, i) {
-        var completeSection = cheerio.load(div).html();
-        var entryDesc = cheerio.load(div)('.result__snippet').text();
-        var entryTitle = cheerio.load(div)('.result__title').text();
-        var href = url.parse(cheerio.load(div)('.result__a').attr('href'), true);
-
-        parsed_links.push({
-          href: href.query.uddg,
-          description: duckClean(entryDesc),
-          title: duckClean(entryTitle)
-        });
+      parsed_links.push({
+        href: href,
+        description: duckClean(entryDesc),
+        title: duckClean(entryTitle),
+        search_category: [searchQuery],
       });
-      /* Remind, in this way I'm losing the association between source
-       * of results and the actual responses.  */
-      return parsed_links;
     });
+    /* Remind, in this way I'm losing the association between source
+     * of results and the actual responses.  */
+    return parsed_links;
+  });
   // TODO manage network error
 };
 
 var mightyDucky = function(val, {log}) {
-  var ddgTerms = envelope.queriesByType('ddg_search', val);
+  var ddgTerms = envelope.queriesByType("ddg_search", val);
 
-  return flowP([
-    collectP(goGoDuck),
-    function(results) {
-      return _.reduce(results, function(flattened, other) {
-        return flattened.concat(other);
-      }, []);
-    },
-    tapP(function(aggregated) {
-      log.debug("Fetched " + _.size(aggregated) + " results for " +
-                _.size(ddgTerms) + " search terms.");
-      _.each(aggregated, function(lo)  {
-        var finalized = _.pick(lo, sharedFields);
-        finalized._sc_source = 'ddg_search';
-        finalized._sc_id_fields = ['href'];
-        finalized._sc_content_fields = ['description'];
-        finalized._sc_media = [{term: lo.href, type: 'url'}],
-        finalized._sc_relations = [{type: 'url', term: lo.href}];
+  return flowP(
+    [
+      collectP(goGoDuck),
+      function(results) {
+        return _.reduce(
+          results,
+          function(flattened, other) {
+            return flattened.concat(other);
+          },
+          [],
+        );
+      },
+      tapP(function(aggregated) {
+        log.debug(
+          "Fetched " +
+            _.size(aggregated) +
+            " results for " +
+            _.size(ddgTerms) +
+            " search terms.",
+        );
+        _.each(aggregated, function(lo) {
+          var finalized = _.pick(lo, sharedFields);
+          finalized._sc_source = "ddg_search";
+          finalized._sc_id_fields = ["href"];
+          finalized._sc_content_fields = ["description"];
+          (finalized._sc_media = [{term: lo.href, type: "url"}]),
+            (finalized._sc_relations = [{type: "url", term: lo.href}]);
 
-        val.data.push(finalized);
-      })
-    }),
-    function () { return val; },
-  ], ddgTerms);
+          val.data.push(finalized);
+        });
+      }),
+      function() {
+        return val;
+      },
+    ],
+    ddgTerms,
+  );
 };
 
-mightyDucky.desc = 'Fetch search results from DuckDuckGo.';
+mightyDucky.desc = "Fetch search results from DuckDuckGo.";
 
 module.exports = {
   plugins: {
