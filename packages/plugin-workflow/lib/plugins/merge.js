301d3f9677ad8225e64be8fafc830eeea934c2d1
Christo <christo@cryptodrunks.net>
2018-10-29 16:16:34 +0100

    feat(plugin-workflow): merge JSON data into units

--- packages/plugin-workflow/lib/plugins/merge.js
+++ packages/plugin-workflow/lib/plugins/merge.js
@@ -1,4 +1,4 @@
-import {mergeAll} from "lodash/fp";
+import {isString, mergeAll} from "lodash/fp";
 import {envelope as env} from "@sugarcube/core";
 
 const querySource = "workflow_merge";
@@ -6,7 +6,7 @@ const querySource = "workflow_merge";
 const plugin = (envelope, {log}) => {
   const queries = env.queriesByType(querySource, envelope);
 
-  const xs = queries.map(q => JSON.parse(q));
+  const xs = queries.map(q => (isString(q) ? JSON.parse(q) : q));
 
   xs.forEach(x => log.info(`Merging ${JSON.stringify(x)} into data.`));
 
