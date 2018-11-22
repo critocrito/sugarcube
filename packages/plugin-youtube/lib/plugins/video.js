82e1fe6baaeeec27ba2bf7793df8bfd05e5ac6bd
Christo <christo@cryptodrunks.net>
2018-11-22 10:10:10 +0100

    feat(plugin-youtube): chunk video downloads in batches of 50

--- packages/plugin-youtube/lib/plugins/video.js
+++ packages/plugin-youtube/lib/plugins/video.js
@@ -1,21 +1,30 @@
-import {get} from "lodash/fp";
-import {flowP, tapP} from "dashp";
-import {plugin as p, envelope as env} from "@sugarcube/core";
+import {get, chunk} from "lodash/fp";
+import {collectP, flowP, tapP, delayP} from "dashp";
+import {envelope as env, plugin as p} from "@sugarcube/core";
 import {videosList} from "../api";
 import {assertCredentials, parseVideoQuery} from "../utils";
 
 const querySource = "youtube_video";
 
-const fetchVideos = (envelope, {cfg, log}) => {
+const fetchVideos = async (envelope, {cfg, log}) => {
   const key = get("youtube.api_key", cfg);
 
-  const scrapeVideo = flowP([
-    parseVideoQuery,
-    tapP(query => log.info(`Fetch details for ${query}.`)),
-    videosList(key),
-  ]);
+  const queries = env
+    .queriesByType(querySource, envelope)
+    .map(term => parseVideoQuery(term));
 
-  return env.flatMapQueriesAsync(scrapeVideo, querySource, envelope);
+  log.info(`Querying for ${queries.length} videos.`);
+
+  const videos = await collectP(
+    flowP([
+      tapP(chunks => log.info(`Fetch details for ${chunks.length} videos.`)),
+      videosList(key),
+      delayP(1000),
+    ]),
+    chunk(50, queries),
+  );
+
+  return env.concatData(videos, envelope);
 };
 
 const plugin = p.liftManyA2([assertCredentials, fetchVideos]);
