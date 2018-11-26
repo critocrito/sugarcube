a9229148b3f68b5e388dd0a34706e6cc2e3149f1
Christo <christo@cryptodrunks.net>
2018-11-26 16:46:29 +0100

    feat(plugin-facebook): catch failures for api pages and populate the failed stats

--- packages/plugin-facebook/lib/plugins/api-pages.js
+++ packages/plugin-facebook/lib/plugins/api-pages.js
@@ -1,22 +1,42 @@
 import {size, get} from "lodash/fp";
 import {flowP, flatmapP} from "dashp";
-import {envelope as e, plugin as p} from "@sugarcube/core";
+import {envelope as env, plugin as p} from "@sugarcube/core";
 
 import {fetchByAppToken, page} from "../api";
 import {assertAppCredentials} from "../assertions";
 
 const querySource = "facebook_page";
 
-const apiPages = (envelope, {log, cfg}) => {
+const apiPages = (envelope, {log, cfg, stats}) => {
   const appId = get("facebook.app_id", cfg);
   const appSecret = get("facebook.app_secret", cfg);
-  const queries = e.queriesByType(querySource, envelope);
+  const queries = env.queriesByType(querySource, envelope);
   const fetcher = fetchByAppToken(appId, appSecret);
 
   log.debug(`Found ${size(queries)} queries.`);
 
+  const queryPage = async q => {
+    let data = [];
+    try {
+      data = await page(fetcher, q);
+    } catch (e) {
+      const fail = {
+        type: querySource,
+        term: q,
+        plugin: "facebook_api_pages",
+        reason: e.message,
+      };
+      stats.update(
+        "failed",
+        fails => (Array.isArray(fails) ? fails.concat(fail) : [fail]),
+      );
+      log.warn(`Facebook page ${q} returned an error: ${e.message}`);
+    }
+    return data;
+  };
+
   return flowP(
-    [flatmapP(page(fetcher)), rs => e.concatData(rs, envelope)],
+    [flatmapP(queryPage), rs => env.concatData(rs, envelope)],
     queries,
   );
 };
