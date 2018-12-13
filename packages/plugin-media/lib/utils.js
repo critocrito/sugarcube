08cfa975b36c4d1e246dabdd74efa09a00514f6d
Christo <christo@cryptodrunks.net>
2018-12-13 11:25:17 +0100

    refactor(plugin-media): remove promise-child-process dep and duplicate code

--- packages/plugin-media/lib/utils.js
+++ packages/plugin-media/lib/utils.js
@@ -1,5 +1,19 @@
-import {spawn} from "child-process-promise";
+import {spawn} from "child_process";
 import {retry} from "dashp";
+
+const doit = (cmd, args) =>
+  // eslint-disable-next-line promise/avoid-new
+  new Promise((resolve, reject) => {
+    const run = spawn(cmd, args);
+    const errMsg = [];
+
+    run.stderr.on("data", d => errMsg.push(d.toString()));
+    run.on("error", () => {
+      const msg = errMsg.map(m => m.trim().replace(/\n$/, "")).join(" ");
+      reject(new Error(msg));
+    });
+    run.on("close", () => resolve());
+  });
 
 export const youtubeDl = (cmd, videoFormat, href, target) => {
   const args = [
@@ -14,44 +28,13 @@ export const youtubeDl = (cmd, videoFormat, href, target) => {
     target,
   ];
 
-  return retry(
-    () =>
-      // eslint-disable-next-line promise/avoid-new
-      new Promise(async (resolve, reject) => {
-        const promise = spawn(cmd, args);
-        const errMsg = [];
-
-        promise.childProcess.stderr.on("data", d => errMsg.push(d.toString()));
-
-        try {
-          await promise;
-          resolve();
-        } catch (e) {
-          const msg = errMsg.map(m => m.trim().replace(/\n$/, "")).join(" ");
-          reject(new Error(msg));
-        }
-      }),
-  );
+  return retry(() => doit(cmd, args));
 };
 
 export const youtubeDlCheck = (cmd, href) => {
   const args = ["-s", href];
 
-  // eslint-disable-next-line promise/avoid-new
-  return new Promise(async (resolve, reject) => {
-    const promise = spawn(cmd, args);
-    const errMsg = [];
-
-    promise.childProcess.stderr.on("data", d => errMsg.push(d.toString()));
-
-    try {
-      await promise;
-      resolve();
-    } catch (e) {
-      const msg = errMsg.map(m => m.trim().replace(/\n$/, "")).join(" ");
-      reject(new Error(msg));
-    }
-  });
+  return doit(cmd, args);
 };
 
 export default {
